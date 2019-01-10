6ec0c48a02df2cc37ffe6bffa379a55ee16c2344
daksh2298 <daksh2298@icloud.com>
2019-01-10 12:50:36 +0530

    Initial commit

--- project/com/controller/RegisterController.py
+++ project/com/controller/RegisterController.py
@@ -1,15 +1,3 @@
 from flask import *
 from project import app
 from project.com.dao import conn_db
-
-@app.route('/')
-def index():
-    return render_template('home.html')
-
-
-@app.route('/home')
-def home():
-    conn=conn_db()
-    cursor=conn.cursor()
-    print(cursor.execute('SELECT * FROM restaurants'))
-    return render_template('home.html')
--- project/com/controller/dashboardController.py
+++ project/com/controller/dashboardController.py
@@ -3,16 +3,49 @@ from project import app
 from project.com.dao import conn_db
 from project.com.dao.dashboardDao import dashboard
 
+sidebar = {'home': None, 'byactivity': None, 'bytime': None, 'byarea': None}
+
+
+@app.route('/')
+def index():
+    sidebar['home'] = True
+    sidebar['byactivity'] = False
+    sidebar['bytime'] = False
+    sidebar['byarea'] = False
+    return render_template('home.html', sidebar=sidebar)
+
+
+@app.route('/home')
+def home():
+    sidebar['home'] = True
+    sidebar['byactivity'] = False
+    sidebar['bytime'] = False
+    sidebar['byarea'] = False
+    conn = conn_db()
+    cursor = conn.cursor()
+    print(cursor.execute('SELECT * FROM restaurants'))
+    return render_template('home.html', sidebar=sidebar)
+
+
 @app.route('/byactivity',methods=['POST','GET'])
 def viewActivity():
     if request.method=='GET':
-        return render_template('byactivity.html',tables=0)
+        sidebar['home'] = False
+        sidebar['byactivity'] = True
+        sidebar['bytime'] = False
+        sidebar['byarea'] = False
+        return render_template('byactivity.html', tables=0, sidebar=sidebar)
     else:
         act=request.form.get('act')
         byact=dashboard()
         data=byact.empByActivity(act)
         print(data)
-        return render_template('byactivity.html',tables=1,emps=data)
+        sidebar['home'] = False
+        sidebar['byactivity'] = True
+        sidebar['bytime'] = False
+        sidebar['byarea'] = False
+        print(sidebar)
+        return render_template('byactivity.html', tables=1, emps=data, sidebar=sidebar)
 
 # @app.route('/byactivity/<act>')
 # def viewActivity(act):
